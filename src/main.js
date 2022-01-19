b2bf28c01f9ba3641711cc856f8985d1120245b3
Aymara <aymara.g.e@gmail.com>
2022-01-19 00:36:50 -0300

    avances en main js y css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import { example } from './data.js';
+//import { example } from './data.js';//
 import data from './data/ghibli/ghibli.js';
 
-const fimls= data.peliculas;
+const movies= data.films;
+const container = document.getElementById("boxfilms");
 
-const movMod= document.getElementById("boxfilms");
+const img=(movies, i) =>{ 
+return `<div>
+    <img src="${movies.poster}" id="${i}" class= "listPoster"/>
+</div> `;
+};
+for(let i= 0; i < movies.length; i++){
+    container.innerHTML += img(movies[i], i);
+  }
 
-console.log(example, data);
+const images= document.getElementsByClassName("listPoster");
+for (let i = 0; i < images.length; i++) {
+  const element = images[i];
+  console.log(element);
+}
+
+
