48fab8434d12d02328f3b3283aa67c558cf78aee
MonicaCofre <cofrev@gmail.com>
2022-01-27 16:51:13 -0300

    tomando datos desde data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
 
-console.log(example, data);
+import data from './data/athletes/athletes.js';
+let xs = data.athletes;
+console.log(xs);
+
+let root = document.getElementById("table");
+
+// window.addEventListener("load", addAllCharacters(xs));
+
+    for (let i =0; i < xs.length; i++){
+        let tarjeta = document.createElement("div");
+        let name= document.createElement("p");
+
+        tarjeta.setAttribute("class", "cell"); 
+        tarjeta.setAttribute("id", "card"); 
+        
+        name.innerHTML = xs[i].name;
+            
+        root.appendChild(tarjeta);
+        tarjeta.appendChild(name);
+    }
+
+
+
+
+
