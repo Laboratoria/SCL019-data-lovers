b807981acff190eb39441596602619e02f0789c6
Angela Rosas Venegas <73150391+arosasvenegas@users.noreply.github.com>
2022-01-31 12:38:09 -0300

    cambie xs por athletes

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 // import { example } from './data.js';
 
 import data from './data/athletes/athletes.js';
-let xs = data.athletes;
-console.log(xs);
+let athletes = data.athletes;
+console.log(athletes);
 
 let root = document.getElementById("table");
 
 // window.addEventListener("load", addAllCharacters(xs));
 
-    for (let i =0; i < xs.length; i++){
+    for (let i =0; i < athletes.length; i++){
         let tarjeta = document.createElement("div");
         let name= document.createElement("p");
 
         tarjeta.setAttribute("class", "cell"); 
         tarjeta.setAttribute("id", "card"); 
         
-        name.innerHTML = xs[i].name;
+        name.innerHTML = athletes[i].name;
             
         root.appendChild(tarjeta);
         tarjeta.appendChild(name);
