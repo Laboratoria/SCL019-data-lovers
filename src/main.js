c2102444c133de03f11c60dd077ca59a6d3ffce5
Angela Rosas Venegas <73150391+arosasvenegas@users.noreply.github.com>
2022-02-01 09:14:52 -0300

    agregando funciones

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // import { example } from './data.js';
 
+import { filterGenero } from './data.js';
 import data from './data/athletes/athletes.js';
 let athletes = data.athletes;
 console.log(athletes);
@@ -7,21 +8,35 @@ console.log(athletes);
 let root = document.getElementById("table");
 
 // window.addEventListener("load", addAllCharacters(xs));
+const card = (data) => {
+    let tarjeta = document.createElement("div");
+    let name= document.createElement("p");
 
-    for (let i =0; i < athletes.length; i++){
-        let tarjeta = document.createElement("div");
-        let name= document.createElement("p");
+    name.innerHTML = ''
+    for (let i =0; i < data.length; i++){
 
         tarjeta.setAttribute("class", "cell"); 
         tarjeta.setAttribute("id", "card"); 
         
-        name.innerHTML = athletes[i].name;
+        name.innerHTML += data[i].name;
             
         root.appendChild(tarjeta);
         tarjeta.appendChild(name);
     }
+    
+    }
 
+    window.addEventListener('load',()=>{
+        card(athletes)
+    })
 
-
+//filtro genero
+ const femFiltro = document.querySelector('#fem');
+ femFiltro.addEventListener('click', ()=>{
+    let resultado = filterGenero(athletes,'F')
+    card(resultado)
+    console.log(resultado)
+     
+ });
 
 
