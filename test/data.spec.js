40f5a8911fe58d07fd8cbc967150e78dd8d495a5
Angela Rosas Venegas <73150391+arosasvenegas@users.noreply.github.com>
2022-02-03 10:23:39 -0300

    arreglo de test, test corriendo :)

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,71 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterGenero } from '../src/data.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const data = [
+  {
+    age: 21,
+    event: "Taekwondo Women's Flyweight",
+    gender: "F",
+    height: "165",
+    medal: "Bronze",
+    name: "Patimat Abakarova",
+    noc: "AZE",
+    sport: "Taekwondo",
+    team: "Azerbaijan",
+    weight: "49"
+  },
+  {
+    age: 31,
+    event: "Rowing Women's Quadruple Sculls",
+    gender: "F",
+    height: "172",
+    medal: "Silver",
+    name: "Chantal Achterberg",
+    noc: "NED",
+    sport: "Rowing",
+    team: "Netherlands",
+    weight: "72"
+  },
+  {
+    age: 21,
+    event: "Rowing Men's Coxless Pairs",
+    gender: "M",
+    height: "198",
+    medal: "Bronze",
+    name: "Giovanni Abagnale",
+    noc: "ITA",
+    sport: "Rowing",
+    team: "Italy",
+    weight: "90",
+  }
+]
+describe('filterGenero', () => {
+  it('deberia ser una funcion', () => {
+    expect(typeof filterGenero).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `filterGenero`', () => {
+    expect(filterGenero(data,'M')).toEqual([{
+    age: 21,
+    event: "Rowing Men's Coxless Pairs",
+    gender: "M",
+    height: "198",
+    medal: "Bronze",
+    name: "Giovanni Abagnale",
+    noc: "ITA",
+    sport: "Rowing",
+    team: "Italy",
+    weight: "90"
+  }]);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+//describe('anotherExample', () => {
+  //it('is a function', () => {
+    //expect(typeof anotherExample).toBe('function');
+  //});
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+  //it('returns `anotherExample`', () => {
+    //expect(anotherExample()).toBe('OMG');
+  //});
+//});
